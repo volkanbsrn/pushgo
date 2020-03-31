cf667b200a03ebbdaddaea681eaf14417d06ecc1
Omer Kirk <omerkirk@Omers-MacBook-Pro.local>
2020-03-31 18:01:02 +0300

    add aditional logging

--- huawei/service.go
+++ huawei/service.go
@@ -129,6 +129,9 @@ func (s *Service) getAccessToken() (string, int, error) {
 }
 
 func (s *Service) Queue(msg *core.Message) {
+	if s.accessToken == "" {
+		return
+	}
 	deviceGroups := core.DeviceList(msg.Devices).Group(1000)
 	data, err := json.Marshal(msg.Json)
 	if err != nil {
@@ -138,7 +141,7 @@ func (s *Service) Queue(msg *core.Message) {
 
 	for i := 0; i < len(deviceGroups); i++ {
 		hcmMsg := hcm.NewMessage(deviceGroups[i], string(data), strconv.Itoa(msg.Expiration), s.isProduction, msg.Extra)
-		log.Printf("pushgo huawei - message: %+v", hcmMsg)
+		log.Printf("pushgo huawei - message: %+v", *hcmMsg)
 		s.msgQueue <- hcmMsg
 	}
 }
@@ -164,6 +167,7 @@ func (s *Service) sender(senderID int) {
 				if err != nil {
 					log.Println("pushgo error: ", err)
 				} else {
+					log.Printf("pushgo huawei - received response: %+v", *resp)
 					s.respCh <- core.NewHCMResponse(resp, m)
 					log.Printf("pushgo: sender %d pushed response of msg with extra %+v to response channel\n", senderID, msg.Extra())
 				}
